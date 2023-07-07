c376f9d305b7f210c84310adb47cc4fc06a67da3
lucas <lucas@phala.network>
2023-07-07 23:01:22 +0800

    add prisma and postgres sql

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '@/utils/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const users = await prisma.user.findMany()
+  console.log(users)
+  res.status(200).json({ name: users })
 }
